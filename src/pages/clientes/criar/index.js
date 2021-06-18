440e771d98e2e46ea91a65c1e4b9106f9bdf7f03
lucasmaranduba <45425616+lucasmaranduba@users.noreply.github.com>
2021-06-17 23:32:43 -0300

    Update index.js

--- src/pages/clientes/criar/index.js
+++ src/pages/clientes/criar/index.js
@@ -120,7 +120,7 @@ class CriarProduto extends Component {
         const value = target.value;
  
         this.setState(prevState => ({
-            usuario: { ...prevState.usuario, [name]: value }
+            produto: { ...prevState.produto, [name]: value }
         }));
         console.log(value);
     };
